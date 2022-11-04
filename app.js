fc8885d7963efabeacf32d4ac4236835a504b3e0
Thomas Williams <cree83@msn.com>
2022-11-03 23:01:32 -0400

    completed exercises

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // TODO: Enable strict mode
+"use strict";
 
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
+  try {
   return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+  } catch (err) {
+    // TODO: if an exception is raised
+    // print the error to the console
+    // and return null
+    console.error(err);
+    return null;
+  }
 }
 
 let failData = "<h1>Hello World!</h1>"
